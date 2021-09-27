9c874c049293bd0b9ef86eed93e9ba2b8dc5cf7e
cwilso88 <chasity.wilson4@gmail.com>
2021-09-27 02:04:45 -0400

    Search menu temp included

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import SearchBarMenu from "./components/SearchBarMenu";
+import './styles/main.scss';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{padding:'20px 60px'}}>
       <header className="header">
-       
+       <SearchBarMenu />
       </header>
     </div>
   );
