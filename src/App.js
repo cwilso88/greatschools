297122e89bd5116b2ff8e5ecfefbccb8a88baa65
cwilso88 <chasity.wilson4@gmail.com>
2021-09-27 04:16:38 -0400

    page  padding

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import './styles/main.scss';
 function App() {
   const mainStyle = {
     background: '#f2f2f2',
-    marginTop: '50px',
+    marginTop: '30px',
     borderTop: '1px solid #d7d6d6'
   }
 
 
   return (
     <div className="App">
-      <header className="header" style={{padding:'20px 60px'}}> 
+      <header className="header" style={{padding:'40px 60px'}}> 
        <SearchBarMenu />
        <FilterOptions />
       </header>
       <main style={mainStyle}>
+        <div style={{padding: '0 60px 0 60px'}}>
 hello
+        </div>
       </main>
     </div>
   );
