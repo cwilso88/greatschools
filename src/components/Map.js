f378eed2ee406c3dcff332e4ebf5d257cbc23fa8
cwilso88 <chasity.wilson4@gmail.com>
2021-09-28 08:37:22 -0400

    updated main image

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Segment } from 'semantic-ui-react';
+import classImg from '../styles/images/class.jpeg'
 
 const Map = () => {
     return(
     <Segment.Group>
-      <Segment>Map</Segment>
+      <Segment>
+        <img className="main_img" src={classImg} alt="class" loading="eager" height="525" width="533" />
+      </Segment>
     </Segment.Group>
     )
 }
