a4ac0d4dde4e0b4fe6139b875529b952469d29fc
cwilso88 <chasity.wilson4@gmail.com>
2021-09-28 08:44:43 -0400

    updated main image size

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,9 +4,9 @@ import classImg from '../styles/images/class.jpeg'
 
 const Map = () => {
     return(
-    <Segment.Group>
+    <Segment.Group id="map">
       <Segment>
-        <img className="main_img" src={classImg} alt="class" loading="eager" height="525" width="533" />
+        <img src={classImg} alt="class" loading="eager" height="575" width="533" />
       </Segment>
     </Segment.Group>
     )
