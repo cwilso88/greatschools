d3a1da17c40973eb2b664d240178de52a4bae59c
cwilso88 <chasity.wilson4@gmail.com>
2021-09-28 13:36:05 -0400

    icon updated

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,10 +3,16 @@ import { Segment } from 'semantic-ui-react';
 import classImg from '../styles/images/class.jpeg'
 
 const Map = () => {
+  const image = {
+      display: "block",
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "100%"
+  }
     return(
     <Segment.Group id="map">
       <Segment>
-        <img src={classImg} alt="class" loading="eager" height="575" width="533" />
+        <img style={image} src={classImg} alt="class" loading="eager" height="575" width="583" />
       </Segment>
     </Segment.Group>
     )
