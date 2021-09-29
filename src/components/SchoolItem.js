6a6c1be88df76e1bc38e40b51a9012704cb4c2eb
cwilso88 <chasity.wilson4@gmail.com>
2021-09-29 00:37:37 -0400

    circle rating styles

--- src/components/SchoolItem.js
+++ src/components/SchoolItem.js
@@ -2,13 +2,66 @@ import React from 'react'
 
 
 const SchoolItem = () => {
+    const circle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontWeight: "700",
+        borderRadius: "50%",
+        height: "50px",
+        width: "50px",
+        textAlign: "center",
+        fontSize: "25px",
+        color: `$darkGreen`,
+        borderStyle: "solid",
+        borderWidth: "4px"
+    }
   return (
     <div id="schoolItem" className="school-list-item">
-      <div className="content-container">
-          <h2>hi</h2>
+      <div className="content-container" style={{display:"flex", gap:"10px"}}>
         <div>
-          <div className="circle-rating">9</div>
+          <div className="circle-rating" style={circle}>9</div>
         </div>
+        <div className="school-info">
+  <a href="/georgia/atlanta/2465-Charles-R.-Drew-Charter-School/">
+    Charles R. Drew Charter School
+  </a>
+  <p className="school-district">
+    <a href="/georgia/atlanta/atlanta-public-schools/">
+      Atlanta Public Schools
+    </a>
+  </p>
+  <p className="students">Charter, PK-5 | 994 students</p>
+  <div className="five-star-review">
+    <span>
+      <a href="/georgia/atlanta/2465-Charles-R.-Drew-Charter-School/reviews">
+        36 Reviews
+      </a>
+    </span>
+    <span>
+      <div className="five-star-rating">
+        <div className="five-star-rating__stars fsr-medium">
+          <span className="five-stars">
+            <span className="icon-star filled-star" />
+            <span className="icon-star filled-star" />
+            <span className="icon-star filled-star" />
+            <span className="icon-star filled-star" />
+            <span className="icon-star empty-star" />
+          </span>
+        </div>
+      </div>
+    </span>
+  </div>
+  <div style={{ marginTop: 5 }}>
+    <a
+      className="icon icon-house active"
+      href="https://www.zillow.com/GA-30317?cbpartner=Great+Schools&utm_source=Great_Schools&utm_medium=referral&utm_campaign=districtpage_schoollistings"
+      target="_blank" rel="noreferrer"
+    >
+      <span> Homes for sale</span>
+    </a>
+  </div>
+</div>
       </div>
     </div>
   );
