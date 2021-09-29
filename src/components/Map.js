eb87f0fd8138a3c1057a6415b57a41bf3c0568f8
cwilso88 <chasity.wilson4@gmail.com>
2021-09-28 23:13:32 -0400

    styles updated for search bar icon and border

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Segment } from 'semantic-ui-react';
-import classImg from '../styles/images/class.jpeg'
+import classImg from '../styles/images/grads.jpeg'
 
 const Map = () => {
   const image = {
@@ -9,12 +9,14 @@ const Map = () => {
       marginRight: "auto",
       width: "100%",
       borderTopLeftRadius: "50px",
-      borderBottomRightRadius: "50px"
+      borderBottomRightRadius: "50px", 
+      boxShadow: `0px 1px 20px 1px #2b6777`,
+      transform: "translate(0, 10px)"
   }
     return(
     <Segment.Group id="map">
       <Segment>
-        <img style={image} src={classImg} alt="class" loading="eager" height="475" width="483" />
+        <img style={image} src={classImg} alt="class" loading="eager" height="475" width="223" />
       </Segment>
     </Segment.Group>
     )
