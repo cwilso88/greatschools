f3d7d259dcc7a8e6292c8b36ee4f427b348a1896
cwilso88 <chasity.wilson4@gmail.com>
2021-10-02 06:00:49 -0400

    changed to class based comp

--- src/components/SchoolList.js
+++ src/components/SchoolList.js
@@ -2,18 +2,14 @@ import React from 'react'
 import { Segment } from 'semantic-ui-react'
 import SchoolItem from './SchoolItem';
 
-const SchoolList = () => (
-    <Segment.Group id="schoolList">
-      <Segment style={{boxShadow: `1px 0px 12px -3px rgba(0,0,0,0.75)`}}>
-        <SchoolItem />
-      </Segment>
-      <Segment>School</Segment>
-      <Segment>School</Segment>
-      <Segment>School</Segment>
-      <Segment>School</Segment>
-      <Segment>School</Segment>
-      <Segment>School</Segment>
-    </Segment.Group>
-)
-
-export default SchoolList;
+export default class SchoolList extends React.Component {
+  render() {
+    return (
+      <Segment.Group id="schoolList">
+        <Segment style={{boxShadow: `1px 0px 12px -3px rgba(0,0,0,0.75)`}}>
+          <SchoolItem />
+        </Segment>
+      </Segment.Group>
+  )
+  }
+}
